baa9791a88f7ccad0ebd5a07756c227bbc6df2b8
Wachimingo <45985242+Wachimingo@users.noreply.github.com>
2022-02-24 08:19:25 -0600

    Update (n)_middleware.ts

--- pages/projects/comedor/(n)_middleware.ts
+++ pages/projects/comedor/(n)_middleware.ts
@@ -1,7 +1,1 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest, event: NextFetchEvent) {
-    if (Object.keys(req.cookies).length < 1) {
-        return NextResponse.redirect('/auth/signin')
-    }
-}

25f4e302399f30cdb098e8505eaa714915a9d13f
Wachimingo <joshuaguillen.adoc@live.com>
2022-03-05 09:58:08 -0600

    added lazyonload flag to script for google

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,8 @@ function MyApp({ Component, pageProps }) {
   }, [])
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} type="text/partytown" />
-      <Script type="text/partytown">
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} type="text/partytown" />
+      <Script strategy="lazyOnload" type="text/partytown">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
